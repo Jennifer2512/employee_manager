241fa66e6fe2ad86d0fb7f9823740a6244de02b7
Jenni <86919247+Jennifer2512@users.noreply.github.com>
2022-03-14 10:51:05 +0700

    Update secondFirebase.js

--- src/secondFirebase.js
+++ src/secondFirebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const config = {
-	apiKey: 'AIzaSyACrvO6gcfne3zKMQH5CzS_NrBRfSIHLoE',
-	authDomain: 'employee-manager-349ef.firebaseapp.com',
-	projectId: 'employee-manager-349ef',
-	storageBucket: 'employee-manager-349ef.appspot.com',
-	messagingSenderId: '584044555183',
-	appId: '1:584044555183:web:65ed1881da3847f2c49d5a'
+	apiKey: '...',
+	authDomain: '...',
+	projectId: '...',
+	storageBucket: '...',
+	messagingSenderId: '...',
+	appId: '...'
 };
 
 var secondaryApp = initializeApp(config, 'Secondary');
